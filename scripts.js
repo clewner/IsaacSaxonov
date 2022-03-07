bbc19ef898f1b918a1778098206aa4699e74ca43
Isaac Saxonov <89880964+clewner@users.noreply.github.com>
2022-03-06 20:55:59 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,3 +15,69 @@ function reveal() {
   }
   
 window.addEventListener("scroll", reveal);
+
+
+var timeouta = setTimeout(function(){
+  window.location.href = "#firstscroll";
+  clearTimeout(timeouta);
+}, 0);
+
+var timeoutb = setTimeout(function(){
+  document.documentElement.scrollTop = 0;
+  clearTimeout(timeoutb);
+}, 500);
+
+var timeoutc = setTimeout(function(){
+  $('#loader').fadeOut("slow");
+  
+  document.getElementById('body').style.overflowY = "visible";
+  clearTimeout(timeoutc);
+}, 2000);
+
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  $("button").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
